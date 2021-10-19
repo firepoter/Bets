e996abea3963941cd858b4ae25e733c9e5d7a292
Ye Xiaoliang <xye002@ikasle.ehu.eus>
2021-10-19 13:29:28 +0200

    coverage

---
+++ src/test/java/Register2BLMockTest.java
@@ -0,0 +1,101 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+
+import businessLogic.BLFacade;
+import businessLogic.BLFacadeImplementation;
+import dataAccess.DataAccess;
+import domain.Admin;
+import domain.Bezeroa;
+import domain.Event;
+import domain.Langilea;
+import domain.Pertsona;
+import exceptions.UserAlreadyExist;
+
+class Register2BLMockTest {
+
+	DataAccess dataAccess = Mockito.mock(DataAccess.class);
+
+	BLFacade sut = new BLFacadeImplementation(dataAccess);
+
+	@Test
+	@DisplayName("Test 4:....")
+	void test4() throws UserAlreadyExist {
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+			Date dat;
+			dat = sdf.parse("19/03/1990");
+			Mockito.when(dataAccess.register(Mockito.any(String.class), Mockito.any(String.class), Mockito.any(String.class), Mockito.any(String.class), Mockito.any(String.class), Mockito.any(String.class), Mockito.any(String.class), Mockito.any(Date.class), Mockito.any(String.class))).thenThrow(UserAlreadyExist.class);
+			assertThrows(UserAlreadyExist.class, () -> sut.register("Josu", "Loidi", "Gorostidi", "Josulo", "aaaaaaaa", "123456789", "josulo@gmail.com", dat, "admin"));
+		} catch (ParseException e) {
+			fail("it should be good");
+		}
+	}
+	
+	@Test
+	@DisplayName("Test 1:...")
+	void test1() throws UserAlreadyExist {
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+			Date dat;
+			dat = sdf.parse("19/03/1990");
+			String cs = "domain.Admin";
+			
+			Mockito.doReturn(new Admin("Josu", "Loidi", "Gorostidi", "Josulo", "aaaaaaaa", "123456789", "josulo@gmail.com", dat)).when(dataAccess).register(Mockito.any(String.class), Mockito.any(String.class), Mockito.any(String.class), Mockito.any(String.class), Mockito.any(String.class), Mockito.any(String.class), Mockito.any(String.class), Mockito.any(Date.class), Mockito.any(String.class));
+			Pertsona berria = dataAccess.register("Josu", "Loidi", "Gorostidi", "Josulo", "aaaaaaaa", "123456789", "josulo@gmail.com", dat, "admin");
+			
+			assertEquals(berria.getClass().getName(), cs);
+			assertEquals("Josu", berria.izena);
+		} catch (ParseException e) {
+			fail("it should be good");
+		}
+	}
+	
+	@Test
+	@DisplayName("Test 2:...")
+	void test2() throws UserAlreadyExist {
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+			Date dat;
+			dat = sdf.parse("10/07/1999");
+			
+			Mockito.doReturn(new Langilea("Jose", "Garcia", "Perez", "JoseRamon", "aaaaaaaa", "123456789", "JoseRamon@gmail.com", dat)).when(dataAccess).register(Mockito.any(String.class), Mockito.any(String.class), Mockito.any(String.class), Mockito.any(String.class), Mockito.any(String.class), Mockito.any(String.class), Mockito.any(String.class), Mockito.any(Date.class), Mockito.any(String.class));
+			Pertsona berria = dataAccess.register("Jose", "Garcia", "Perez", "JoseRamon", "aaaaaaaa", "123456789", "JoseRamon@gmail.com", dat, "langilea");
+			
+			String cs = "domain.Langilea";
+			assertEquals(berria.getClass().getName(), cs);
+			assertEquals("Jose", berria.izena);
+		} catch (ParseException e) {
+			fail("it should be good");
+		}
+	}
+	
+	@Test
+	@DisplayName("Test 3:...")
+	void test3() throws UserAlreadyExist {
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+			Date dat;
+			dat = sdf.parse("20/11/2000");
+			
+			Mockito.doReturn(new Bezeroa("Saioa", "Goikoetxea", "Ugarte", "Saioo99", "b", "123456789", "Saioo99@gmail.com", dat)).when(dataAccess).register(Mockito.any(String.class), Mockito.any(String.class), Mockito.any(String.class), Mockito.any(String.class), Mockito.any(String.class), Mockito.any(String.class), Mockito.any(String.class), Mockito.any(Date.class), Mockito.any(String.class));
+			Pertsona berria = dataAccess.register("Saioa", "Goikoetxea", "Ugarte", "Saioo99", "b", "123456789", "Saioo99@gmail.com", dat, "bezeroa");
+			
+			String cs = "domain.Bezeroa";
+			assertEquals(berria.getClass().getName(), cs);
+			assertEquals("Saioa", berria.izena);
+		} catch (ParseException e) {
+			fail("it should be good");
+		}
+	}
+
+
+}
---
+++ src/test/java/Register2DATest2.java
@@ -0,0 +1,110 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import configuration.ConfigXML;
+import dataAccess.DataAccess;
+import domain.Pertsona;
+import exceptions.QuestionAlreadyExist;
+import exceptions.UserAlreadyExist;
+import utility.TestUtilityDataAccess;
+
+class Register2DATest2 {
+
+	static DataAccess sut = new DataAccess(ConfigXML.getInstance().getDataBaseOpenMode().equals("initialize"));;
+	static TestUtilityDataAccess testDA = new TestUtilityDataAccess();
+
+	Pertsona berria;
+	
+	@Test
+	@DisplayName("Test 1:...")
+	void test1() {
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+			Date dat;
+			dat = sdf.parse("19/03/1990");
+			
+			testDA.open();
+			testDA.addPertsona();
+			testDA.close();
+			
+			assertThrows(UserAlreadyExist.class, () -> sut.register("Josu", "Loidi", "Gorostidi", "Josulo", "aaaaaaaa", "123456789", "josulo@gmail.com", dat, "admin"));
+		} catch (ParseException e) {
+			fail("it should be good");
+		}
+	}
+	
+	@Test
+	@DisplayName("Test 2:...")
+	void test2() throws UserAlreadyExist {
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+			Date dat;
+			dat = sdf.parse("19/03/1990");
+			String exIzena = "Juan";
+			
+			berria = sut.register("Juan", "Carlos", "Calvo", "juana123", "123456", "656532320", "juana123@gmail.com", dat, "admin");
+			assertEquals(berria.getIzena(), exIzena);
+			assertEquals(berria.getClass().getName(), "domain.Admin");
+		}catch(ParseException e) {
+			fail("it should be good");
+		}
+	}
+	
+	@Test
+	@DisplayName("Test 3:...")
+	void test3() throws UserAlreadyExist {
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+			Date dat;
+			dat = sdf.parse("10/07/1999");
+			String exIzena = "Pablo";
+			
+			berria = sut.register("Pablo", "Iglesia", "Calvo", "Rem", "aaaaaaaa", "123456789", "Rem@gmail.com", dat, "langilea");
+			assertEquals(berria.getIzena(), exIzena);
+			assertEquals(berria.getClass().getName(), "domain.Langilea");
+		}catch(ParseException e) {
+			fail("it should be good");
+		}
+	}
+	
+	@Test
+	@DisplayName("Test 4:...")
+	void test4() throws UserAlreadyExist {
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+			Date dat;
+			dat = sdf.parse("20/11/2000");
+			String exIzena = "Horo";
+			
+			berria = sut.register("Horo", "Laurence", "Lafurenche", "Horo66", "b", "123456789", "Horo66@gmail.com", dat, "bezeroa");
+			assertEquals(berria.getIzena(), exIzena);
+			assertEquals(berria.getClass().getName(), "domain.Bezeroa");
+		}catch(ParseException e) {
+			fail("it should be good");
+		}
+	}
+	
+	@Test
+	@DisplayName("Test 5:...")
+	void test5() throws UserAlreadyExist {
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+			Date dat;
+			dat = sdf.parse("20/11/2000");
+			
+			assertThrows(IllegalArgumentException.class, ()-> sut.register("Mihail", "Constanting", "Ugarte", "Mihail", "b", "123456789", "Mihail@gmail.com", dat, "ikaslea"));
+		}catch(ParseException e) {
+			fail("it should be good");
+		}
+	}
+	
+	
+
+}
